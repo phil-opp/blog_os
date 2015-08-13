457a61341a3b62c9c99981bf2fa90a333aedc928
Philipp Oppermann <dev@phil-opp.com>
2015-08-13 13:15:37 +0200

    Enable paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -24,6 +24,7 @@ start:
     call check_long_mode
 
     call setup_page_tables
+    call enable_paging
 
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
@@ -37,6 +38,30 @@ setup_page_tables:
 
     ; map first P3 entry to a huge page that starts at address 0
     mov dword [p3_table], 0b10000011 ; present + writable + huge
+
+    ret
+
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
 
     ret
 
