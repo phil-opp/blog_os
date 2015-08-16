66cc22608cd1bc04da765182d2102f41b0c7bda2
Philipp Oppermann <dev@phil-opp.com>
2015-08-16 13:49:46 +0200

    Add a rust project

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+// Copyright 2015 Philipp Oppermann
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![feature(no_std, lang_items)]
+#![no_std]
+
+#[no_mangle]
+pub extern fn main() {
+    // ATTENTION: we have a very small stack and no guard page
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
