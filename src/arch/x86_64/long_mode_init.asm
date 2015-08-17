3c08203437213c25dc21ba479d90baf7039ebad3
Philipp Oppermann <dev@phil-opp.com>
2015-08-17 15:12:22 +0200

    Call rust main function

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -13,10 +13,14 @@
 ; limitations under the License.
 
 global long_mode_start
+extern main
 
 section .text
 bits 64
 long_mode_start:
+
+    ; call rust main
+    call main
     ; print `OKAY` to screen
     mov rax, 0x2f592f412f4b2f4f
     mov qword [0xb8000], rax
