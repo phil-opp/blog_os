03a46b4141dd09ac0719f69eb257bda35dbe5087
Philipp Oppermann <dev@phil-opp.com>
2015-08-17 15:20:09 +0200

    Print `OS returned!` instead of `OKAY`

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -22,9 +22,14 @@ long_mode_start:
 
     ; call rust main
     call main
-    ; print `OKAY` to screen
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
+.os_returned:
+    ; rust main returned, print `OS returned!`
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
     hlt
 
 ; Check for SSE and enable it. If it's not supported throw error "a".
