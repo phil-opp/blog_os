7e5dad227713a6b007f0b907ac2f09db4557c40a
Philipp Oppermann <dev@phil-opp.com>
2015-11-06 23:49:09 +0100

    Update blog on push to master through travis

---
+++ scripts/travis-blog-update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# License: CC0 1.0 Universal
+# https://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+set -e
+
+. ./scripts/travis-doc-upload.cfg
+
+[ "$TRAVIS_BRANCH" = master ]
+
+[ "$TRAVIS_PULL_REQUEST" = false ]
+
+eval SSH_KEY_TRAVIS_ID=aaae456e27e9
+eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
+eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
+
+mkdir -p ~/.ssh
+openssl aes-256-cbc -K $key -iv $iv -in scripts/id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+
+git clone --branch gh-pages ./ deploy_blog
+
+cd deploy_blog
+git config user.name "blog update bot"
+git config user.email "nobody@example.com"
+rm -rf _posts
+rm -rf _pages/*
+cp ../posts _posts
+cp ../pages/* ./
+git add _posts _pages
+git commit -qm "update blog to $TRAVIS_COMMIT"
+git push -q origin gh-pages
