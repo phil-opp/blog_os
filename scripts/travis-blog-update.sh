2691f2b5d7180af30e42252e223b518fbd8ade72
Philipp Oppermann <dev@phil-opp.com>
2015-11-07 00:19:09 +0100

    Final travis fix: add `-r` flag to copy

--- scripts/travis-blog-update.sh
+++ scripts/travis-blog-update.sh
@@ -22,9 +22,7 @@ git clone --branch gh-pages git@github.com:$TRAVIS_REPO_SLUG deploy_blog
 cd deploy_blog
 git config user.name "blog update bot"
 git config user.email "nobody@example.com"
-rm -rf _posts
-rm -rf _pages/*
-cp ../posts _posts
+cp -r ../posts _posts
 cp ../pages/* ./
 git add _posts _pages
 git commit -qm "update blog to $TRAVIS_COMMIT"
