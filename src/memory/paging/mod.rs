6b72dd82231c8e35c3c90bbc941d02fc771a9121
Philipp Oppermann <dev@phil-opp.com>
2015-11-26 10:14:04 +0100

    Remove `Lock` structure again as we changed design

--- src/memory/paging/mod.rs
+++
@@ -1,8 +0,0 @@
-/// The paging lock must be unique. It is required for all page table operations and thus
-/// guarantees exclusive page table access.
-pub struct Lock {
-    _private: (),
-}
-
-impl !Send for Lock {}
-impl !Sync for Lock {}
