953704f10313569dc16c820629b4f9a8645e4685
Philipp Oppermann <dev@phil-opp.com>
2015-12-12 14:21:15 +0100

    Move `setup_SSE` to boot.asm

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -29,6 +29,7 @@ start:
 
     call setup_page_tables
     call enable_paging
+    call setup_SSE
 
     ; load the 64-bit GDT
     lgdt [gdt64.pointer]
@@ -145,6 +146,28 @@ test_long_mode:
     ret
 .no_long_mode:
     mov al, "2"
+    jmp error
+
+; Check for SSE and enable it. If it's not supported throw error "a".
+setup_SSE:
+    ; check for SSE
+    mov eax, 0x1
+    cpuid
+    test edx, 1<<25
+    jz .no_SSE
+
+    ; enable SSE
+    mov eax, cr0
+    and ax, 0xFFFB      ; clear coprocessor emulation CR0.EM
+    or ax, 0x2          ; set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9       ; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
+
+    ret
+.no_SSE:
+    mov al, "a"
     jmp error
 
 section .bss
