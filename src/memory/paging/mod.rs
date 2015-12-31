b64f980c34050256b0c2366121cef95ddf4646ee
Philipp Oppermann <dev@phil-opp.com>
2015-12-31 15:20:34 +0100

    Identity map the vga text buffer

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -216,6 +216,9 @@ pub fn remap_the_kernel<A>(allocator: &mut A, boot_info: &BootInformation)
                 mapper.identity_map(frame, flags, allocator);
             }
         }
+
+        let vga_buffer_frame = Frame::containing_address(0xb8000);
+        mapper.identity_map(vga_buffer_frame, WRITABLE, allocator);
     });
 
     let old_table = active_table.switch(new_table);
