cea73b3ab9aede168214365d47720d7abafec793
Philipp Oppermann <dev@phil-opp.com>
2016-04-11 22:14:04 +0200

    Move memory intialization to memory::init function

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -10,11 +10,39 @@
 pub use self::area_frame_allocator::AreaFrameAllocator;
 pub use self::paging::remap_the_kernel;
 use self::paging::PhysicalAddress;
+use multiboot2::BootInformation;
 
 mod area_frame_allocator;
 mod paging;
 
 pub const PAGE_SIZE: usize = 4096;
+
+pub fn init(boot_info: &BootInformation) {
+    let memory_map_tag = boot_info.memory_map_tag().expect(
+        "Memory map tag required");
+    let elf_sections_tag = boot_info.elf_sections_tag().expect(
+        "Elf sections tag required");
+
+    let kernel_start = elf_sections_tag.sections()
+        .filter(|s| s.is_allocated()).map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections()
+        .filter(|s| s.is_allocated()).map(|s| s.addr + s.size).max()
+        .unwrap();
+
+    println!("kernel start: {:#x}, kernel end: {:#x}",
+             kernel_start,
+             kernel_end);
+    println!("multiboot start: {:#x}, multiboot end: {:#x}",
+             boot_info.start_address(),
+             boot_info.end_address());
+
+    let mut frame_allocator = AreaFrameAllocator::new(
+        kernel_start as usize, kernel_end as usize,
+        boot_info.start_address(), boot_info.end_address(),
+        memory_map_tag.memory_areas());
+
+    paging::remap_the_kernel(&mut frame_allocator, boot_info);
+}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Frame {
