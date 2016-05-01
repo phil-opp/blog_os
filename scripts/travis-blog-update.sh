1e69a0c62780cfea2780d73af7f44a0afb41b645
Philipp Oppermann <dev@phil-opp.com>
2016-05-01 14:24:32 +0200

    Build hugo blog using travis

--- scripts/travis-blog-update.sh
+++ scripts/travis-blog-update.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# License: CC0 1.0 Universal
-# https://creativecommons.org/publicdomain/zero/1.0/legalcode
-
 set -e
 
+# update blog if current branch is `master`
 [ "$TRAVIS_BRANCH" = master ]
-
 [ "$TRAVIS_PULL_REQUEST" = false ]
 
+# decrypt ssh key
 eval SSH_KEY_TRAVIS_ID=aaae456e27e9
 eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
 eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
@@ -17,13 +15,22 @@ mkdir -p ~/.ssh
 openssl aes-256-cbc -K $key -iv $iv -in scripts/travis-blog_os.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
+# clone gh-pages to `deploy_blog`
 git clone --branch gh-pages git@github.com:$TRAVIS_REPO_SLUG deploy_blog
-
 cd deploy_blog
+
+# set git user/email
 git config user.name "travis-update-bot"
 git config user.email "travis-update-bot@phil-opp.com"
-cp  ../posts/* _posts/
-cp ../pages/* ./
-git add .
+
+# update blog
+rm -r *
+cp -r ../hugo/public/* .
+rm -r post post.html page page.html # remove per-category pages/feeds
+
+# commit
+git add --all .
 git commit -qm "Update blog to $TRAVIS_COMMIT"
+
+# push changes
 git push -q origin gh-pages
