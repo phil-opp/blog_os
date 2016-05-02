d056a4115496336c195c9de86c14f646431151ef
Philipp Oppermann <dev@phil-opp.com>
2016-05-02 15:24:20 +0200

    Trigger rebuild of hugo branch instead of deploying ourselves

---
+++ scripts/travis-trigger-hugo-build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+[ "$TRAVIS_BRANCH" = master ]
+[ "$TRAVIS_PULL_REQUEST" = false ]
+
+body='{
+"request": {
+  "branch":"hugo"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $TRAVIS_TOKEN" \
+  -d "$body" \
+  https://api.travis-ci.org/repo/phil-opp%2Fblog_os/requests
