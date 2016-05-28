c65b16f42d27daab392a035e41eca80054122f2a
Philipp Oppermann <dev@phil-opp.com>
2016-05-28 15:26:20 +0200

    Create a static IDT with a page fault handler function

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -1,1 +1,20 @@
 mod idt;
+
+lazy_static! {
+    static ref IDT: idt::Idt = {
+        let mut idt = idt::Idt::new();
+
+        idt.set_handler(14, page_fault_handler);
+
+        idt
+    };
+}
+
+pub fn init() {
+    IDT.load();
+}
+
+extern "C" fn page_fault_handler() -> ! {
+    println!("EXCEPTION: PAGE FAULT");
+    loop {}
+}
