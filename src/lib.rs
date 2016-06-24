dcab71016c342e4757fca62ce70d026eb9416e80
Philipp Oppermann <dev@phil-opp.com>
2016-06-24 13:35:22 +0200

    Create dummy _Unwind_Resume function to fix libcore linker errors
    (cherry picked from commit 952f7ed5e3fe470330d0b5cce4677e4f37f24c1b)

--- src/lib.rs
+++ src/lib.rs
@@ -85,6 +85,7 @@ extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
     loop {}
 }
 
+#[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn _Unwind_Resume() -> ! {
     loop {}
