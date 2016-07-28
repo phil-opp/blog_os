46bb4856213b3e834b323c25d799acc0c58e923c
Philipp Oppermann <dev@phil-opp.com>
2016-07-28 19:49:59 +0200

    Don't do anything if there's no #toc

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,19 @@
 window.onload = function() {
   var container = document.querySelector('#toc');
 
-  var toc = initTOC({
-      selector: 'h2, h3',
-      scope: '.post',
-      overwrite: false,
-      prefix: 'toc'
-  });
+  if (container != null) {
+    var toc = initTOC({
+        selector: 'h2, h3',
+        scope: '.post',
+        overwrite: false,
+        prefix: 'toc'
+    });
 
-  var heading = document.createElement("H2");
-  var heading_text = document.createTextNode("Table of Contents");
-  heading.appendChild(heading_text);
+    var heading = document.createElement("H2");
+    var heading_text = document.createTextNode("Table of Contents");
+    heading.appendChild(heading_text);
 
-  container.appendChild(heading);
-  container.appendChild(toc);
+    container.appendChild(heading);
+    container.appendChild(toc);
+  }
 }
