35c083122ef678d4db4b71ced4985c73ac072abe
Philipp Oppermann <dev@phil-opp.com>
2016-07-28 19:51:36 +0200

    Add function to automatically add/rm the coarse class
    Based on viewport height. Automatically adjusted onresize.

--- static/js/main.js
+++ static/js/main.js
@@ -15,5 +15,26 @@ window.onload = function() {
 
     container.appendChild(heading);
     container.appendChild(toc);
+
+    resize_toc(container);
   }
+}
+
+function resize_toc(container) {
+  var containerHeight = container.clientHeight;
+
+  var resize = function() {
+    if (containerHeight > document.documentElement.clientHeight - 100) {
+      container.classList.add('coarse');
+    } else {
+      container.classList.remove('coarse');
+    }
+  };
+  resize();
+
+  var resizeId;
+  window.onresize = function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(resize, 300);
+  };
 }
