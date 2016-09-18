8cee4161de1aa25654b8a5f04721c4c1afb82a38
Philipp Oppermann <dev@phil-opp.com>
2016-09-18 19:26:45 +0200

    Update to hugo 0.16

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -14,10 +14,10 @@ git clone --branch=hugo https://github.com/phil-opp/blog_os.git hugo
 cd hugo
 
 # download hugo
-wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz
-tar xf hugo_0.15_linux_amd64.tar.gz
+wget https://github.com/spf13/hugo/releases/download/v0.16/hugo_0.16_linux-64bit.tgz
+tar xf hugo_0.16_linux-64bit.tgz
 
 # build the blog
-hugo_0.15_linux_amd64/hugo_0.15_linux_amd64
+./hugo
 
 cd ..
