12b66968dc6490354da0643c56364924cb947236
Philipp Oppermann <dev@phil-opp.com>
2016-10-04 19:15:47 +0200

    Call codegen crate in build script

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# codegen
+cd codegen
+cargo run -- -o "../layouts/partials/recent-updates.html"
+cd ..
+
 git clone https://github.com/phil-opp/blog_os.git
 cp -r blog_os/blog ../blog
 ./hugo
