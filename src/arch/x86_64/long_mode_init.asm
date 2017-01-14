24e9bcb659389ee6de48f50d411367d5b60c96fe
Philipp Oppermann <dev@phil-opp.com>
2017-01-14 16:38:33 +0100

    Load null selectors to all data registers
    This is required for some instructions such as iretq (ss must be valid or 0).
    
    (cherry picked from commit 8f8b46a9b66175280adb4a225050deed2de8fe0a)

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -13,6 +13,14 @@ extern rust_main
 section .text
 bits 64
 long_mode_start:
+    ; load 0 into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
     ; call rust main (with multiboot pointer in rdi)
     call rust_main
 .os_returned:
