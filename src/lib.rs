5d8758df59637fa83d8ce501ef567a0c13165944
Philipp Oppermann <dev@phil-opp.com>
2017-04-11 18:25:51 +0200

    Add a dependency on rlibc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+    let x = ["Hello", "World", "!"];
+    let y = x;
+}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
