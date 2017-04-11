5ebdd89ffb61c144f3226a3b2beb06de3216a013
Philipp Oppermann <dev@phil-opp.com>
2017-04-11 14:50:00 +0200

    Add an `error` function

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -6,3 +6,12 @@ start:
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
