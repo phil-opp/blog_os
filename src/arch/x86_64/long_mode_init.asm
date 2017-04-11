9136a7b66f72426ecc3040502a6145064f0cc5fb
Philipp Oppermann <dev@phil-opp.com>
2017-04-11 14:59:43 +0200

    Zero all segment registers

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -3,6 +3,14 @@ global long_mode_start
 section .text
 bits 64
 long_mode_start:
+    ; load 0 into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
     ; print `OKAY` to screen
     mov rax, 0x2f592f412f4b2f4f
     mov qword [0xb8000], rax
