3706331a4362c8d10619674ebbb8ac600d260c95
Philipp Oppermann <dev@phil-opp.com>
2017-04-13 18:36:07 +0200

    Add a P4 constant and a Table::next_table_address method

--- src/memory/paging/table.rs
+++ src/memory/paging/table.rs
@@ -2,6 +2,8 @@ use core::ops::{Index, IndexMut};
 use memory::paging::entry::*;
 use memory::paging::ENTRY_COUNT;
 
+pub const P4: *mut Table = 0xffffffff_fffff000 as *mut _;
+
 pub struct Table {
     entries: [Entry; ENTRY_COUNT],
 }
@@ -10,6 +12,16 @@ impl Table {
     pub fn zero(&mut self) {
         for entry in self.entries.iter_mut() {
             entry.set_unused();
+        }
+    }
+
+    fn next_table_address(&self, index: usize) -> Option<usize> {
+        let entry_flags = self[index].flags();
+        if entry_flags.contains(PRESENT) && !entry_flags.contains(HUGE_PAGE) {
+            let table_address = self as *const _ as usize;
+            Some((table_address << 9) | (index << 12))
+        } else {
+            None
         }
     }
 }
