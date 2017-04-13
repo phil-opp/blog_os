2b7fa410fbce36a85c2f6a40741366a5baa5f69a
Philipp Oppermann <dev@phil-opp.com>
2017-04-13 18:32:00 +0200

    Implement the Index and IndexMut traits for Table

--- src/memory/paging/table.rs
+++ src/memory/paging/table.rs
@@ -1,6 +1,21 @@
+use core::ops::{Index, IndexMut};
 use memory::paging::entry::*;
 use memory::paging::ENTRY_COUNT;
 
 pub struct Table {
     entries: [Entry; ENTRY_COUNT],
+}
+
+impl Index<usize> for Table {
+    type Output = Entry;
+
+    fn index(&self, index: usize) -> &Entry {
+        &self.entries[index]
+    }
+}
+
+impl IndexMut<usize> for Table {
+    fn index_mut(&mut self, index: usize) -> &mut Entry {
+        &mut self.entries[index]
+    }
 }
