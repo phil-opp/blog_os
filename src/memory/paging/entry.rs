c4c27c10e68b0de502e123c1139d3cf69bf0777a
Philipp Oppermann <dev@phil-opp.com>
2017-04-13 18:24:27 +0200

    Create a memory::paging::entry submodule with an Entry struct

---
+++ src/memory/paging/entry.rs
@@ -0,0 +1,13 @@
+use memory::Frame;
+
+pub struct Entry(u64);
+
+impl Entry {
+    pub fn is_unused(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn set_unused(&mut self) {
+        self.0 = 0;
+    }
+}
--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -1,5 +1,7 @@
 use memory::PAGE_SIZE;
 
+mod entry;
+
 const ENTRY_COUNT: usize = 512;
 
 pub type PhysicalAddress = usize;
