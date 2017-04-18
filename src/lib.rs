fa3b572d6851c6df292d887834a47b50d779eb4d
Philipp Oppermann <dev@phil-opp.com>
2017-04-18 12:22:36 +0200

    Only print kernel/multiboot start/end in rust_main

--- src/lib.rs
+++ src/lib.rs
@@ -26,21 +26,8 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
     let memory_map_tag = boot_info.memory_map_tag()
         .expect("Memory map tag required");
-
-    println!("memory areas:");
-    for area in memory_map_tag.memory_areas() {
-        println!("    start: 0x{:x}, length: 0x{:x}",
-            area.base_addr, area.length);
-    }
-
     let elf_sections_tag = boot_info.elf_sections_tag()
-        .expect("Elf-sections tag required");
-
-    println!("kernel sections:");
-    for section in elf_sections_tag.sections() {
-        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-            section.addr, section.size, section.flags);
-    }
+        .expect("Elf sections tag required");
 
     let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
         .min().unwrap();
@@ -48,6 +35,11 @@ pub extern fn rust_main(multiboot_information_address: usize) {
         .max().unwrap();
     let multiboot_start = multiboot_information_address;
     let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("kernel start: 0x{:x}, kernel end: 0x{:x}",
+        kernel_start, kernel_end);
+    println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}",
+        multiboot_start, multiboot_end);
 
     let mut frame_allocator = memory::AreaFrameAllocator::new(
         kernel_start as usize, kernel_end as usize, multiboot_start,
