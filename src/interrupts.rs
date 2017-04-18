fab320271af050838e7be8739c51e0b82943f6d2
Philipp Oppermann <dev@phil-opp.com>
2017-04-18 15:16:26 +0200

    Create and load an IDT

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,15 @@
 use x86_64::structures::idt::{Idt, ExceptionStackFrame};
 
+lazy_static! {
+    static ref IDT: Idt = {
+        let mut idt = Idt::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
 pub fn init() {
-    let mut idt = Idt::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
