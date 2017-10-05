dae2edfbf5b0b03ad6ebe3f0f16db316d0cba6ba
Philipp Oppermann <dev@phil-opp.com>
2017-10-05 11:37:23 +0200

    Let gutenberg generate the table of contents
    Remove the javascript toc code.

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -2,22 +2,6 @@ window.onload = function() {
   var container = document.querySelector('#toc');
 
   if (container != null) {
-    var toc = initTOC({
-        selector: 'h2, h3',
-        scope: 'main',
-        overwrite: false,
-        prefix: 'toc'
-    });
-
-    toc.innerHTML = toc.innerHTML.split("🔗\n").join("");
-
-    var heading = document.createElement("H2");
-    var heading_text = document.createTextNode("Table of Contents");
-    heading.appendChild(heading_text);
-
-    container.appendChild(heading);
-    container.appendChild(toc);
-
     resize_toc(container);
   }
 }
