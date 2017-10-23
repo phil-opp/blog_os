7fbc4870a8b7e17a1420d65bb5930091b9972ca1
Johan Montelius <johanmon@kth.se>
2017-10-23 21:49:45 +0200

    rewrite where a Heap is protected by LockedHeap

--- libs/bump_allocator/src/lib.rs
+++ libs/bump_allocator/src/lib.rs
@@ -21,44 +21,86 @@ extern crate alloc;
 extern crate spin;
 
 
-static HEAP: Mutex<Heap> = Mutex::new(Heap::new(0, 0));
-
-//Set up the heap
-pub unsafe fn init(offset: usize, size: usize) {
-    *HEAP.lock() = Heap::new(offset, size);
-}
-
-#[derive(Debug)]
-struct Heap {
-    start: usize,
-    end: usize,
-    next: usize,
+struct LockedHeap {
+    heap: Mutex<Heap>,
 }
 
 
-impl Heap {
-    /// Initialisation of the heap to use the 
-    /// range [start, start + size).
-    const fn new(start: usize, size: usize) -> Heap {
-        Heap {
-            start: start,
-            end: start + size,
-            next: start,
+#[global_allocator]
+static GLOBAL_ALLOC: LockedHeap = LockedHeap::empty();
+
+
+pub unsafe fn init(start: usize, size: usize) {
+    GLOBAL_ALLOC.init(start, size);
+}
+
+
+impl LockedHeap {
+    // Creates an empty heap. All allocate calls will return 'AllocErr`.
+    pub const fn empty() -> LockedHeap {
+        LockedHeap {
+            heap : Mutex::new(Heap::empty())
         }
+    }
+
+    unsafe fn init(&self, start: usize, size: usize)  {
+        self.heap.lock().init(start, size);
     }
 }
 
-pub struct Allocator;
+// The interface used for all allocation of heap structures. 
 
-unsafe impl<'a> Alloc for &'a Allocator {
+unsafe impl<'a> Alloc for &'a LockedHeap {
 
     unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
-        let ref mut heap = HEAP.lock();
-        let alloc_start = align_up(heap.next, layout.align());
+        self.heap.lock().allocate(layout)
+    }
+
+    unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+        self.heap.lock().dealloc(ptr, layout)
+    }
+}
+
+
+
+// A fixed size heap with a reference to the beginning of free space.
+
+pub struct Heap {
+    start: usize,
+    end: usize,
+    next:  usize,
+}
+
+impl Heap {
+    // Creates an empty heap. All allocate calls will return `AllocErr`.
+    pub const fn empty() -> Heap {
+        Heap {
+            start: 0,
+            end: 0,
+            next: 0,
+        }
+    }
+
+    // This is unsafe, the start address must be valid and the memory in
+    // the `[start, start + size)` range must not be used for anything
+    // else. This function is unsafe because it can cause undefined
+    // behavior if the given address or size are invalid.
+
+    unsafe fn init(&mut self, start: usize, size: usize) {
+        self.start = start;
+        self.end = start + size;
+        self.next = start;
+    }
+
+    // Allocates a chunk of the given size with the given alignment. Returns a pointer to the
+    // beginning of that chunk if it was successful, else it returns an AllocErr.
+
+    unsafe fn allocate(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
+        let alloc_start = align_up(self.next, layout.align());
         let alloc_end = alloc_start.saturating_add(layout.size());
 
-        if alloc_end <= heap.end {
-            heap.next =  alloc_end;
+        if alloc_end <= self.end {
+            self.next =  alloc_end;
             Ok(alloc_start as *mut u8)
         } else {
             Err(AllocErr::Exhausted{request: layout})
@@ -68,11 +110,11 @@ unsafe impl<'a> Alloc for &'a Allocator {
     unsafe fn dealloc(&mut self, _ptr: *mut u8, _layout: Layout) {
         // Sofar nothing - don't worry, RAM is cheap
     }
-}
+}    
 
 
-/// Align downwards. Returns the greatest x with alignment `align`
-/// so that x <= addr. The alignment must be a power of 2.
+// Align downwards. Returns the greatest x with alignment `align`
+// so that x <= addr. The alignment must be a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
     if align.is_power_of_two() {
         addr & !(align - 1)
@@ -83,12 +125,10 @@ pub fn align_down(addr: usize, align: usize) -> usize {
     }
 }
 
-/// Align upwards. Returns the smallest x with alignment `align`
-/// so that x >= addr. The alignment must be a power of 2.
+// Align upwards. Returns the smallest x with alignment `align`
+// so that x >= addr. The alignment must be a power of 2.
 pub fn align_up(addr: usize, align: usize) -> usize {
     align_down(addr + align - 1, align)
 }
 
-#[global_allocator]
-static GLOBAL_ALLOC: Allocator = Allocator;
 
