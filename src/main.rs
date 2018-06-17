97a87728f70a506a64e1c76f4552e32928642ff0
Philipp Oppermann <dev@phil-opp.com>
2018-06-17 20:08:51 +0200

    Port cpu exceptions post to second edition

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![feature(panic_implementation)] // required for defining the panic handler
+#![feature(abi_x86_interrupt)]
 #![no_std] // don't link the Rust standard library
 #![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 #[macro_use]
 extern crate blog_os;
+extern crate x86_64;
+#[macro_use]
+extern crate lazy_static;
 
 use core::panic::PanicInfo;
 
@@ -15,6 +19,12 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    init_idt();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::int3();
+
+    println!("It did not crash!");
     loop {}
 }
 
@@ -25,4 +35,22 @@ pub extern "C" fn _start() -> ! {
 pub fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+use x86_64::structures::idt::{ExceptionStackFrame, Idt};
+
+lazy_static! {
+    static ref IDT: Idt = {
+        let mut idt = Idt::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
