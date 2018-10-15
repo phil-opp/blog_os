47db96923e193cfa3ae2ba3b7ff1a567899c2a18
acheronfail <acheronfail@gmail.com>
2018-10-16 07:57:36 +1000

    add gifs for keyboard and fix source to compile

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,9 @@ pub unsafe fn exit_qemu() {
     let mut port = Port::<u32>::new(0xf4);
     port.write(0);
 }
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
