218cb9399e8bf925c3556746c630938b24cc59a8
Philipp Oppermann <dev@phil-opp.com>
2019-01-25 13:54:44 +0100

    Add a double fault handler and test it

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -11,6 +11,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -21,4 +22,12 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut ExceptionStackFrame,
+    _error_code: u64,
+) {
+    println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::interrupts::init_idt();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     println!("It did not crash!");
     loop {}
