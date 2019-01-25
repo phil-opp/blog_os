bd3910579388424d885d7d839cd53d213f51ae96
Philipp Oppermann <dev@phil-opp.com>
2019-01-25 13:44:04 +0100

    Split crate into lib.rs and main.rs

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(not(test), no_std)]
+
+pub mod serial;
+pub mod vga_buffer;
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,15 @@
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(unused_imports))]
 
+use blog_os::println;
 use core::panic::PanicInfo;
-
-mod serial;
-mod vga_buffer;
 
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    serial_println!("Hello Host{}", "!");
-
-    unsafe {
-        exit_qemu();
-    }
 
     loop {}
-}
-
-pub unsafe fn exit_qemu() {
-    use x86_64::instructions::port::Port;
-
-    let mut port = Port::<u32>::new(0xf4);
-    port.write(0);
 }
 
 /// This function is called on panic.
