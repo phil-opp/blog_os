8bb0187f35fa069e1a4e2154df4c12dffef2f261
Philipp Oppermann <dev@phil-opp.com>
2019-01-25 14:44:42 +0100

    Run rustfmt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -13,7 +13,8 @@ lazy_static! {
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         unsafe {
-            idt.double_fault.set_handler_fn(double_fault_handler)
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
         idt
