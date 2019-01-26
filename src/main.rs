b184f7d9963d58ba07d1a9bee124113f88e1bf23
Philipp Oppermann <dev@phil-opp.com>
2019-01-26 12:57:32 +0100

    Move the testing code to the end of `_start`

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ pub extern "C" fn _start() -> ! {
     use blog_os::interrupts::PICS;
     use x86_64::structures::paging::PageTable;
 
-    let level_4_table_ptr = 0xffff_ffff_ffff_f000 as *const PageTable;
-    let level_4_table = unsafe { &*level_4_table_ptr };
-    for i in 0..10 {
-        println!("Entry {}: {:?}", i, level_4_table[i]);
-    }
-
     println!("Hello World{}", "!");
 
     blog_os::gdt::init();
     blog_os::interrupts::init_idt();
     unsafe { PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+
+    let level_4_table_ptr = 0xffff_ffff_ffff_f000 as *const PageTable;
+    let level_4_table = unsafe { &*level_4_table_ptr };
+    for i in 0..10 {
+        println!("Entry {}: {:?}", i, level_4_table[i]);
+    }
 
     println!("It did not crash!");
     blog_os::hlt_loop();
