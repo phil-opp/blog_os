67f536d7c6c11be7f10a7f0153e7c44f4c7cc217
Philipp Oppermann <dev@phil-opp.com>
2019-01-28 11:27:52 +0100

    Add a BootInfoFrameAllocator

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,4 @@
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 use x86_64::structures::paging::{
     FrameAllocator, Mapper, Page, PageTable, PhysFrame, RecursivePageTable, Size4KiB,
 };
@@ -18,6 +19,24 @@ pub unsafe fn init(level_4_table_addr: usize) -> RecursivePageTable<'static> {
     }
 
     init_inner(level_4_table_addr)
+}
+
+/// Create a FrameAllocator from the passed memory map
+pub fn init_frame_allocator(
+    memory_map: &'static MemoryMap,
+) -> BootInfoFrameAllocator<impl Iterator<Item = PhysFrame>> {
+    // get usable regions from memory map
+    let regions = memory_map
+        .iter()
+        .filter(|r| r.region_type == MemoryRegionType::Usable);
+    // map each region to its address range
+    let addr_ranges = regions.map(|r| r.range.start_addr()..r.range.end_addr());
+    // transform to an iterator of frame start addresses
+    let frame_addresses = addr_ranges.flat_map(|r| r.into_iter().step_by(4096));
+    // create `PhysFrame` types from the start addresses
+    let frames = frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)));
+
+    BootInfoFrameAllocator { frames }
 }
 
 /// Returns the physical address for the given virtual address, or `None` if
@@ -51,5 +70,21 @@ pub struct EmptyFrameAllocator;
 impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
         None
+    }
+}
+
+pub struct BootInfoFrameAllocator<I>
+where
+    I: Iterator<Item = PhysFrame>,
+{
+    frames: I,
+}
+
+impl<I> FrameAllocator<Size4KiB> for BootInfoFrameAllocator<I>
+where
+    I: Iterator<Item = PhysFrame>,
+{
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        self.frames.next()
     }
 }
