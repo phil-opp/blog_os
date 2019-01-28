f76ec1ae32c2e8022409ebeebd2a19ea2d79a6e7
Philipp Oppermann <dev@phil-opp.com>
2019-01-28 11:50:59 +0100

    Only set double fault handler once

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -12,7 +12,6 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
