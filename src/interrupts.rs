3d208b5973521eec23fc7c83f490d376716a4913
Antoine <antoine.sb@orange.fr>
2019-02-12 17:07:36 +0000

    :recycle: extract the `enum` containing the interrupts out of the submodule
    * rename `enum Hardware` into `HardwareInterruptIndexes` and extract it out of `mod interrupt_indexes`
    * remove `mod interrupt_indexes`
    * adding `Clone` and `Copy` traits to `HardwareInterruptIndexes`
    * implement `as_u8(self)` and `as_usize(self)` functions for `HardwareInterruptIndexes`
    * remove useless `use` directives
    * ensure consistent indentation (spaces, 4)
    * remove Herobrine

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -13,15 +13,16 @@ use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable};
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
-pub mod interrupt_indexes {
-    #[derive(Debug)]
-    #[repr(u8)]
-    pub enum Hardware {
-        Timer = super::PIC_1_OFFSET,
-        Keyboard,
-    }
-    pub fn as_u8(interrupt: Hardware) -> u8 { interrupt as u8 }
-    pub fn as_usize(interrupt: Hardware) -> usize { usize::from(as_u8(interrupt)) }
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum HardwareInterruptIndexes {
+    Timer = PIC_1_OFFSET,
+    Keyboard,
+}
+
+impl HardwareInterruptIndexes {
+    fn as_u8(self) -> u8 { self as u8 }
+    fn as_usize(self) -> usize { usize::from(self.as_u8()) }
 }
 
 pub static PICS: spin::Mutex<ChainedPics> =
@@ -36,9 +37,9 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-        use self::interrupt_indexes::*;
-        idt[as_usize(Hardware::Timer)].set_handler_fn(timer_interrupt_handler);
-        idt[as_usize(Hardware::Keyboard)].set_handler_fn(keyboard_interrupt_handler);
+        use self::HardwareInterruptIndexes::*;
+        idt[Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -61,12 +62,10 @@ extern "x86-interrupt" fn double_fault_handler(
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
     print!(".");
-    use self::interrupt_indexes::*;
-    unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Timer)) }
+    unsafe { PICS.lock().notify_end_of_interrupt(HardwareInterruptIndexes::Timer.as_u8()) }
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
-    use interrupt_indexes::*;
     use pc_keyboard::{layouts, DecodedKey, Keyboard, ScancodeSet1};
     use spin::Mutex;
     use x86_64::instructions::port::Port;
@@ -89,5 +88,5 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Exceptio
         }
     }
 
-    unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Keyboard)) }
+    unsafe { PICS.lock().notify_end_of_interrupt(HardwareInterruptIndexes::Keyboard.as_u8()) }
 }
