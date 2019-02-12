8d82d9b2debf627f1849fd57d9b1ff5edaaf8969
Antoine <antoine.sb@orange.fr>
2019-02-12 16:03:17 +0000

    :pencil2: fuck spaces

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -15,13 +15,13 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub mod interrupt_indexes {
     #[derive(Debug)]
-	#[repr(u8)]
-	pub enum Hardware {
-		Timer = super::PIC_1_OFFSET,
-		Keyboard,
-	}
-	pub fn as_u8(interrupt: Hardware) -> u8 { interrupt as u8 }
-	pub fn as_usize(interrupt: Hardware) -> usize { usize::from(as_u8(interrupt)) }
+    #[repr(u8)]
+    pub enum Hardware {
+        Timer = super::PIC_1_OFFSET,
+        Keyboard,
+    }
+    pub fn as_u8(interrupt: Hardware) -> u8 { interrupt as u8 }
+    pub fn as_usize(interrupt: Hardware) -> usize { usize::from(as_u8(interrupt)) }
 }
 
 pub static PICS: spin::Mutex<ChainedPics> =
@@ -36,9 +36,9 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-	    use self::interrupt_indexes::*;
-	    idt[as_usize(Hardware::Timer)].set_handler_fn(timer_interrupt_handler);
-	    idt[as_usize(Hardware::Keyboard)].set_handler_fn(keyboard_interrupt_handler);
+        use self::interrupt_indexes::*;
+        idt[as_usize(Hardware::Timer)].set_handler_fn(timer_interrupt_handler);
+        idt[as_usize(Hardware::Keyboard)].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
