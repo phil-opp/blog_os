af83146d335d2b00263011a9a1c8c32d849a8b8a
Antoine <antoine.sb@orange.fr>
2019-02-12 15:59:33 +0000

    :pencil2: fix more indentation mistakes

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -14,7 +14,7 @@ pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub mod interrupt_indexes {
-	#[derive(Debug)]
+    #[derive(Debug)]
 	#[repr(u8)]
 	pub enum Hardware {
 		Timer = super::PIC_1_OFFSET,
@@ -36,9 +36,9 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-	use self::interrupt_indexes::*;
-	idt[as_usize(Hardware::Timer)].set_handler_fn(timer_interrupt_handler);
-	idt[as_usize(Hardware::Keyboard)].set_handler_fn(keyboard_interrupt_handler);
+	    use self::interrupt_indexes::*;
+	    idt[as_usize(Hardware::Timer)].set_handler_fn(timer_interrupt_handler);
+	    idt[as_usize(Hardware::Keyboard)].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -62,11 +62,11 @@ extern "x86-interrupt" fn double_fault_handler(
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
     print!(".");
     use self::interrupt_indexes::*;
-	unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Timer)) }
+    unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Timer)) }
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut ExceptionStackFrame) {
-	use interrupt_indexes::*;
+    use interrupt_indexes::*;
     use pc_keyboard::{layouts, DecodedKey, Keyboard, ScancodeSet1};
     use spin::Mutex;
     use x86_64::instructions::port::Port;
@@ -89,5 +89,5 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Exceptio
         }
     }
 
-	unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Keyboard)) }
+    unsafe { PICS.lock().notify_end_of_interrupt(as_u8(Hardware::Keyboard)) }
 }
