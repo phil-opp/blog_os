3b13211579fcd1f195f7a1acda3b71f862bc50ae
Philipp Oppermann <dev@phil-opp.com>
2019-04-22 11:52:22 +0200

    Add the skeleton for an integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
