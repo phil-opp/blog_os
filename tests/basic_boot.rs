a2d628cabe80af76a83b702a88cca20e2c52505d
Philipp Oppermann <dev@phil-opp.com>
2019-04-22 13:20:22 +0200

    Complete the basic_boot integration test

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
@@ -13,11 +13,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    blog_os::test_panic_handler(info)
 }
