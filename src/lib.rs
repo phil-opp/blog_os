c8821cb226e57721a84d279de6f69c6e42b75356
Philipp Oppermann <dev@phil-opp.com>
2019-04-26 12:00:51 +0200

    Use `entry_point` macro in lib.rs too

--- src/lib.rs
+++ src/lib.rs
@@ -57,10 +57,15 @@ pub fn hlt_loop() -> ! {
     }
 }
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
