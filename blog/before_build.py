c1994c943f36ba5acd9d9acd9f1039129f6afa41
Philipp Oppermann <dev@phil-opp.com>
2019-05-03 19:35:52 +0200

    Show the 10 most recent changes (instead of 5)

--- blog/before_build.py
+++ blog/before_build.py
@@ -11,7 +11,7 @@ with io.open("templates/recent-updates.html", 'w', encoding='utf8') as recent_up
 
     recent_updates.write(u"<ul>\n")
 
-    for pr in g.search_issues("is:merged", repo="phil-opp/blog_os", type="pr", label="relnotes")[:5]:
+    for pr in g.search_issues("is:merged", repo="phil-opp/blog_os", type="pr", label="relnotes")[:10]:
         link = '<a href="' + pr.html_url + '">' + pr.title + "</a> "
         iso_date = pr.closed_at.isoformat()
         readable_date = pr.closed_at.strftime("%b&nbsp;%d")
