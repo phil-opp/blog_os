e2f088170122b46c7c5ff05b13938405782708eb
Philipp Oppermann <dev@phil-opp.com>
2019-05-06 11:40:56 +0200

    Show only changes of last month in recent updates list

--- blog/before_build.py
+++ blog/before_build.py
@@ -3,15 +3,23 @@
 
 import io
 from github import Github
+from datetime import datetime, timedelta
 
 g = Github()
+
+one_month_ago = datetime.now() - timedelta(days=32)
+
+def filter_date(issue):
+    return issue.closed_at > one_month_ago
 
 with io.open("templates/recent-updates.html", 'w', encoding='utf8') as recent_updates:
     recent_updates.truncate()
 
     recent_updates.write(u"<ul>\n")
 
-    for pr in g.search_issues("is:merged", repo="phil-opp/blog_os", type="pr", label="relnotes")[:10]:
+    issues = g.search_issues("is:merged", repo="phil-opp/blog_os", type="pr", label="relnotes")[:10]
+
+    for pr in filter(filter_date, issues):
         link = '<a href="' + pr.html_url + '">' + pr.title + "</a> "
         iso_date = pr.closed_at.isoformat()
         readable_date = pr.closed_at.strftime("%b&nbsp;%d")
