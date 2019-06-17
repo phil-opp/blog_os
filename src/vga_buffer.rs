fcd849f471fc08df9ceb8dd6c49e3b0e279ef18a
Philipp Oppermann <dev@phil-opp.com>
2019-06-17 17:28:27 +0200

    Fix deprecated range syntax

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -112,7 +112,7 @@ impl Writer {
         for byte in s.bytes() {
             match byte {
                 // printable ASCII byte or newline
-                0x20...0x7e | b'\n' => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 // not part of printable ASCII range
                 _ => self.write_byte(0xfe),
             }
