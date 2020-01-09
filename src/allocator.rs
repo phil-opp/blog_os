08d2289dad5be8862392ec191c5bc2f71a3139b5
Philipp Oppermann <dev@phil-opp.com>
2020-01-09 15:34:04 +0100

    Add a `Locked` wrapper type that can be used to implement GlobalAlloc

--- src/allocator.rs
+++ src/allocator.rs
@@ -54,3 +54,20 @@ unsafe impl GlobalAlloc for Dummy {
         panic!("dealloc should be never called")
     }
 }
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
