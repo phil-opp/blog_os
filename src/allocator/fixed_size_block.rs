4f234b67ef279c5d3f249ffca19bf5a1839ffcbc
Philipp Oppermann <dev@phil-opp.com>
2020-01-20 14:02:57 +0100

    Add ListNode type and BLOCK_SIZES constant

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,1 +1,9 @@
+/// The block sizes to use.
+///
+/// The sizes must each be power of 2 because they are also used as
+/// the block alignment (alignments must be always powers of 2).
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 
+struct ListNode {
+    next: Option<&'static mut ListNode>,
+}
