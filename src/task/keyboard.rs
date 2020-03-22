05f6abd261aa8b4f7adbffb2eb5a575ddfea8dea
Philipp Oppermann <dev@phil-opp.com>
2020-03-22 12:10:10 +0100

    Fill the scancode queue on keyboard interrupts

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,4 +1,18 @@
+use crate::println;
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+
+/// Called by the keyboard interrupt handler
+///
+/// Must not block or allocate.
+pub(crate) fn add_scancode(scancode: u8) {
+    if let Ok(queue) = SCANCODE_QUEUE.try_get() {
+        if let Err(_) = queue.push(scancode) {
+            println!("WARNING: scancode queue full; dropping keyboard input");
+        }
+    } else {
+        println!("WARNING: scancode queue uninitialized");
+    }
+}
