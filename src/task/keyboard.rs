3a2a468a0b70c72c67bf6121dc33f8603d16b502
Philipp Oppermann <dev@phil-opp.com>
2020-03-22 12:12:11 +0100

    Add a ScancodeStream based on the SCANCODE_QUEUE

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,6 +1,11 @@
 use crate::println;
 use conquer_once::spin::OnceCell;
+use core::{
+    pin::Pin,
+    task::{Context, Poll},
+};
 use crossbeam_queue::ArrayQueue;
+use futures_util::stream::Stream;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
@@ -14,5 +19,30 @@ pub(crate) fn add_scancode(scancode: u8) {
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
+    }
+}
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE
+            .try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE.try_get().expect("not initialized");
+        match queue.pop() {
+            Ok(scancode) => Poll::Ready(Some(scancode)),
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
     }
 }
