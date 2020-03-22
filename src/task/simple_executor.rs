0186f65ece6abcf68dfef53977a87c907cdf8ea1
Philipp Oppermann <dev@phil-opp.com>
2020-03-22 17:24:58 +0100

    Use cooked-waker crate for creating wakers

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,23 +1,24 @@
 use super::Task;
-use crate::println;
-use alloc::collections::{BTreeMap, VecDeque};
-use conquer_once::spin::OnceCell;
-use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use alloc::{
+    collections::{BTreeMap, VecDeque},
+    sync::Arc,
+};
+use cooked_waker::IntoWaker;
+use core::task::{Context, Poll};
 use crossbeam_queue::ArrayQueue;
-
-static WAKE_QUEUE: OnceCell<ArrayQueue<usize>> = OnceCell::uninit();
 
 pub struct SimpleExecutor {
     task_queue: VecDeque<Task>,
     waiting_tasks: BTreeMap<usize, Task>,
+    wake_queue: Arc<ArrayQueue<usize>>,
 }
 
 impl SimpleExecutor {
     pub fn new() -> SimpleExecutor {
-        WAKE_QUEUE.init_once(|| ArrayQueue::new(100));
         SimpleExecutor {
             task_queue: VecDeque::new(),
             waiting_tasks: BTreeMap::new(),
+            wake_queue: Arc::new(ArrayQueue::new(100)),
         }
     }
 
@@ -33,7 +34,7 @@ impl SimpleExecutor {
     }
 
     fn handle_wakeups(&mut self) {
-        while let Ok(task_id) = WAKE_QUEUE.get().unwrap().pop() {
+        while let Ok(task_id) = self.wake_queue.pop() {
             if let Some(task) = self.waiting_tasks.remove(&task_id) {
                 self.task_queue.push_back(task);
             }
@@ -42,7 +43,11 @@ impl SimpleExecutor {
 
     fn run_ready_tasks(&mut self) {
         while let Some(mut task) = self.task_queue.pop_front() {
-            let waker = waker(task.id());
+            let waker = TaskWaker {
+                task_id: task.id(),
+                wake_queue: self.wake_queue.clone(),
+            }
+            .into_waker();
             let mut context = Context::from_waker(&waker);
             match task.poll(&mut context) {
                 Poll::Ready(()) => {} // task done
@@ -56,25 +61,26 @@ impl SimpleExecutor {
     }
 }
 
-fn raw_waker(task_id: usize) -> RawWaker {
-    fn clone(id: *const ()) -> RawWaker {
-        raw_waker(id as usize)
-    }
-
-    fn wake(id: *const ()) {
-        if let Err(_) = WAKE_QUEUE.try_get().unwrap().push(id as usize) {
-            println!("WARNING: WAKE_QUEUE full; dropping wakeup")
-        }
-    }
-
-    fn drop(_id: *const ()) {}
-
-    RawWaker::new(
-        task_id as *const (),
-        &RawWakerVTable::new(clone, wake, wake, drop),
-    )
+#[derive(Debug, Clone, IntoWaker)]
+struct TaskWaker {
+    task_id: usize,
+    wake_queue: Arc<ArrayQueue<usize>>,
 }
 
-fn waker(task_id: usize) -> Waker {
-    unsafe { Waker::from_raw(raw_waker(task_id)) }
+impl TaskWaker {
+    fn wake_task(&self) {
+        self.wake_queue.push(self.task_id).expect("wake queue full");
+    }
+}
+
+impl cooked_waker::WakeRef for TaskWaker {
+    fn wake_by_ref(&self) {
+        self.wake_task();
+    }
+}
+
+impl cooked_waker::Wake for TaskWaker {
+    fn wake(self) {
+        self.wake_task();
+    }
 }
