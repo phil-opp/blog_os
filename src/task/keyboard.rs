71b10a70dff5c7d8a90e692ed0c37adf47edacda
Philipp Oppermann <dev@phil-opp.com>
2020-03-27 13:14:57 +0100

    Add dependencies on crossbeam and conquer_once to define a SCANCODE_QUEUE

---
+++ src/task/keyboard.rs
@@ -0,0 +1,4 @@
+use conquer_once::spin::OnceCell;
+use crossbeam_queue::ArrayQueue;
+
+static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
--- src/task/mod.rs
+++ src/task/mod.rs
@@ -5,6 +5,7 @@ use core::{
     task::{Context, Poll},
 };
 
+pub mod keyboard;
 pub mod simple_executor;
 
 pub struct Task {
