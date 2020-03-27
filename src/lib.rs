dac7e67403784d0ed58a63265648e0dfca7b675a
Philipp Oppermann <dev@phil-opp.com>
2020-03-27 12:55:40 +0100

    Add a task module with a Task struct

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub mod gdt;
 pub mod interrupts;
 pub mod memory;
 pub mod serial;
+pub mod task;
 pub mod vga_buffer;
 
 pub fn init() {
---
+++ src/task/mod.rs
@@ -0,0 +1,22 @@
+use alloc::boxed::Box;
+use core::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+}
+
+impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
+        Task {
+            future: Box::pin(future),
+        }
+    }
+
+    fn poll(&mut self, context: &mut Context) -> Poll<()> {
+        self.future.as_mut().poll(context)
+    }
+}
