b862534be5066f28ea0a21ddcb666ff392f2ed44
Philipp Oppermann <dev@phil-opp.com>
2020-04-01 12:53:25 +0200

    Use a static counter for assigning task IDs (#782)
    Deriving the task ID from the heap address of the future does not work for zero-sized futures because they are not backed by a real allocation.

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -2,6 +2,7 @@ use alloc::boxed::Box;
 use core::{
     future::Future,
     pin::Pin,
+    sync::atomic::{AtomicU64, Ordering},
     task::{Context, Poll},
 };
 
@@ -10,12 +11,14 @@ pub mod keyboard;
 pub mod simple_executor;
 
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
         Task {
+            id: TaskId::new(),
             future: Box::pin(future),
         }
     }
@@ -23,14 +26,14 @@ impl Task {
     fn poll(&mut self, context: &mut Context) -> Poll<()> {
         self.future.as_mut().poll(context)
     }
-
-    fn id(&self) -> TaskId {
-        use core::ops::Deref;
-
-        let addr = Pin::deref(&self.future) as *const _ as *const () as usize;
-        TaskId(addr)
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct TaskId(usize);
+struct TaskId(u64);
+
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+    }
+}
