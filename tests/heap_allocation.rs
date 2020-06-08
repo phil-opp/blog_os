e7fd27b648c074ff4fb39c05f70afc934e22fb74
Philipp Oppermann <dev@phil-opp.com>
2020-06-08 12:08:25 +0200

    Remove superfluous printing from heap_allocation tests

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -7,7 +7,7 @@
 extern crate alloc;
 
 use alloc::{boxed::Box, vec::Vec};
-use blog_os::{allocator::HEAP_SIZE, serial_print, serial_println};
+use blog_os::allocator::HEAP_SIZE;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
@@ -30,34 +30,28 @@ fn main(boot_info: &'static BootInfo) -> ! {
 
 #[test_case]
 fn simple_allocation() {
-    serial_print!("simple_allocation... ");
     let heap_value_1 = Box::new(41);
     let heap_value_2 = Box::new(13);
     assert_eq!(*heap_value_1, 41);
     assert_eq!(*heap_value_2, 13);
-    serial_println!("[ok]");
 }
 
 #[test_case]
 fn large_vec() {
-    serial_print!("large_vec... ");
     let n = 1000;
     let mut vec = Vec::new();
     for i in 0..n {
         vec.push(i);
     }
     assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
-    serial_println!("[ok]");
 }
 
 #[test_case]
 fn many_boxes() {
-    serial_print!("many_boxes... ");
     for i in 0..HEAP_SIZE {
         let x = Box::new(i);
         assert_eq!(*x, i);
     }
-    serial_println!("[ok]");
 }
 
 #[panic_handler]
