80bfeb24a9a005064cc52d32d7783725e4327377
Philipp Oppermann <dev@phil-opp.com>
2021-02-23 11:33:13 +0100

    Create an EFI entry point function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+#![feature(abi_efiapi)]
+
 #![no_std]
 #![no_main]
 
+use core::ffi::c_void;
 use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "efiapi" fn efi_main(image: *mut c_void, system_table: *const c_void) -> usize {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
