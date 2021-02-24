443dff95582f692d2b0a48996eb29fc03a4a5008
Philipp Oppermann <dev@phil-opp.com>
2021-02-24 11:18:54 +0100

    Create a GPT disk image

--- disk_image/src/main.rs
+++ disk_image/src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs, io, path::Path};
+use std::{
+    convert::TryFrom,
+    fs::{self, File},
+    io::{self, Seek},
+    path::Path,
+};
 
 fn main() {
     println!("Hello, world!");
@@ -33,4 +38,51 @@ fn create_fat_filesystem(fat_path: &Path, efi_file: &Path) {
     let mut bootx64 = root_dir.create_file("efi/boot/bootx64.efi").unwrap();
     bootx64.truncate().unwrap();
     io::copy(&mut fs::File::open(&efi_file).unwrap(), &mut bootx64).unwrap();
+}
+
+fn create_gpt_disk(disk_path: &Path, fat_image: &Path) {
+    // create new file
+    let mut disk = fs::OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .read(true)
+        .write(true)
+        .open(&disk_path)
+        .unwrap();
+
+    // set file size
+    let partition_size: u64 = fs::metadata(&fat_image).unwrap().len();
+    let disk_size = partition_size + 1024 * 64; // for GPT headers
+    disk.set_len(disk_size).unwrap();
+
+    // create a protective MBR at LBA0 so that disk is not considered
+    // unformatted on BIOS systems
+    let mbr = gpt::mbr::ProtectiveMBR::with_lb_size(
+        u32::try_from((disk_size / 512) - 1).unwrap_or(0xFF_FF_FF_FF),
+    );
+    mbr.overwrite_lba0(&mut disk).unwrap();
+
+    // create new GPT structure
+    let block_size = gpt::disk::LogicalBlockSize::Lb512;
+    let mut gpt = gpt::GptConfig::new()
+        .writable(true)
+        .initialized(false)
+        .logical_block_size(block_size)
+        .create_from_device(Box::new(&mut disk), None)
+        .unwrap();
+    gpt.update_partitions(Default::default()).unwrap();
+
+    // add new EFI system partition and get its byte offset in the file
+    let partition_id = gpt
+        .add_partition("boot", partition_size, gpt::partition_types::EFI, 0)
+        .unwrap();
+    let partition = gpt.partitions().get(&partition_id).unwrap();
+    let start_offset = partition.bytes_start(block_size).unwrap();
+
+    // close the GPT structure and write out changes
+    gpt.write().unwrap();
+
+    // place the FAT filesystem in the newly created partition
+    disk.seek(io::SeekFrom::Start(start_offset)).unwrap();
+    io::copy(&mut File::open(&fat_image).unwrap(), &mut disk).unwrap();
 }
