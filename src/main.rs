e0b5cf89ba0647402b6a5d905d5aa67a96c83497
Philipp Oppermann <dev@phil-opp.com>
2021-03-19 14:41:16 +0100

    Use `uefi` crate and its `entry` macro

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![feature(abi_efiapi)]
-
 #![no_std]
 #![no_main]
 
-use core::ffi::c_void;
 use core::panic::PanicInfo;
+use uefi::prelude::entry;
 
-#[no_mangle]
-pub extern "efiapi" fn efi_main(image: *mut c_void, system_table: *const c_void) -> usize {
+#[entry]
+fn efi_main(
+    image: uefi::Handle,
+    system_table: uefi::table::SystemTable<uefi::table::Boot>,
+) -> uefi::Status {
     loop {}
 }
 
