9d78913a37481b697086c04d2e812a8de172a1aa
Philipp Oppermann <dev@phil-opp.com>
2021-03-24 13:32:19 +0100

    Set up allocations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![feature(abi_efiapi)]
+#![feature(alloc_error_handler)]
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::{alloc::Layout, fmt::Write, panic::PanicInfo};
 use uefi::prelude::entry;
 
 #[entry]
@@ -10,10 +14,29 @@ fn efi_main(
     image: uefi::Handle,
     system_table: uefi::table::SystemTable<uefi::table::Boot>,
 ) -> uefi::Status {
+    let stdout = system_table.stdout();
+    stdout.clear().unwrap().unwrap();
+    writeln!(stdout, "Hello World!").unwrap();
+
+    unsafe {
+        uefi::alloc::init(system_table.boot_services());
+    }
+
+    writeln!(stdout, "alloc").unwrap();
+    let mut v: Vec<u32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    writeln!(stdout, "v = {:?}", v).unwrap();
+
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    panic!("out of memory")
 }
