a9e8caed904bb1d0b153512e357e0fd93a076c44
Philipp Oppermann <dev@phil-opp.com>
2021-04-05 13:04:05 +0200

    The wake trait is stable now

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 #![feature(abi_x86_interrupt)]
 #![feature(alloc_error_handler)]
 #![feature(const_mut_refs)]
-#![feature(wake_trait)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
