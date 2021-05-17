d55fa1ee2497c4407920fc6f2f20bfc149f7906b
Philipp Oppermann <dev@phil-opp.com>
2021-05-17 11:46:24 +0200

    Adjust post-06 to take `InterruptStackFrame` by value

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -24,7 +24,7 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 }
 
 extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: &mut InterruptStackFrame,
+    stack_frame: InterruptStackFrame,
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
