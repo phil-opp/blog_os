4b49e71db8e29c450a37fc54d2acfcbb43c497b4
jeevan-spec <73019593+jeevan-spec@users.noreply.github.com>
2022-10-22 14:57:24 +0530

    Create program_to_draw_heart.py

---
+++ program_to_draw_heart.py
@@ -0,0 +1,34 @@
+import turtle
+pen=turtle.Turtle()
+def curve():
+ for i in range(200):
+   pen.right(1)
+   pen.forward(1)
+
+def heart():
+
+ pen.fillcolor('red')
+
+ pen.begin_fill()
+
+ pen.left(140)
+ pen.forward(113)
+
+ curve()
+ pen.left(120)
+
+ curve()
+ pen.forward(112)
+ pen.end_fill()
+
+def txt():
+
+ pen.up()
+ pen.setpos(-68,95)
+ pen.down()
+ pen.color('lightgreen')
+ pen.write("HELLO WORLD", font=(
+ "Verdana",12,"bold"))
+heart()
+txt()
+pen.ht()
