73bb171e4f2656a91a479f83b61b65aa3353e1b9
Philipp Oppermann <dev@phil-opp.com>
2023-03-25 18:57:41 +0100

    Define a `_start` entry point function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@
 
 use core::panic::PanicInfo;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
