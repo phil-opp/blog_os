cf28a5fdf95c0bf66d8a9b45bc32ae11a6eacc86
Philipp Oppermann <dev@phil-opp.com>
2023-05-01 15:05:34 +0200

    Print something to the screen

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,9 +3,18 @@
 
 use core::panic::PanicInfo;
 
+use bootloader_api::BootInfo;
+
 bootloader_api::entry_point!(kernel_main);
 
-fn kernel_main(_bootinfo: &'static mut bootloader_api::BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        let mut value = 0x90;
+        for byte in framebuffer.buffer_mut() {
+            *byte = value;
+            value = value.wrapping_add(1);
+        }
+    }
     loop {}
 }
 
