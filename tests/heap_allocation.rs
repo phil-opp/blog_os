38aab28025015c618bd903ccbf7314f61a656076
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-01-01 14:23:57 +0000

    Correcting many_boxes_long_lived that BumpAllocator should fail, when it currently succeeds with cargo test -r

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -7,6 +7,7 @@
 extern crate alloc;
 
 use alloc::{boxed::Box, vec::Vec};
+use core::hint::black_box;
 use blog_os::allocator::HEAP_SIZE;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
@@ -61,6 +62,7 @@ fn many_boxes_long_lived() {
         let x = Box::new(i);
         assert_eq!(*x, i);
     }
+    black_box(&long_lived); // new - ensures long_lived isn't optimized away
     assert_eq!(*long_lived, 1); // new
 }
 
