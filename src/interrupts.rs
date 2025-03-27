e9e75e759ebd1d7ca369c94fd836c359ea57cf1a
Philipp Oppermann <dev@phil-opp.com>
2025-03-27 16:13:37 +0100

    Run cargo fmt for post-07

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -66,7 +66,7 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use pc_keyboard::{DecodedKey, HandleControl, Keyboard, ScancodeSet1, layouts};
     use spin::Mutex;
     use x86_64::instructions::port::Port;
 
