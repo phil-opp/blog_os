e4b3ed6d42d6f77d6894e65dd3edd4b378efe512
Philipp Oppermann <dev@phil-opp.com>
2025-03-27 15:13:34 +0100

    Fix: `no_mangle` is an unsafe attribute since Rust 2024

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     test_main();
     loop {}
